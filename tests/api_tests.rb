3c1d7f702bbf7f17c36e921ff356fd71434530b3
Angel Nuñez <snahider@gmail.com>
2019-05-23 13:11:30 -0500

    mejor nombre api test

--- tests/api_tests.rb
+++ tests/api_tests.rb
@@ -1,7 +1,7 @@
 require 'airborne'
 
 describe 'reloj api' do
-  it 'retorna si la alarma esta sonando' do
+  it 'el API retorna si la alarma esta sonando' do
     get 'https://relojdespertador.herokuapp.com/api/alarma/sonar?hora-alarma=07:20&hora-actual=07:19'
     expect_json(sonar: false)
   end
